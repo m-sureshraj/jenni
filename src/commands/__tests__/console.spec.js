fd0d8f4594ea172004c848ff9740992af3b75e43
Sureshraj <m.s.suresh100@gmail.com>
2020-08-03 13:25:01 +0530

    feat(console-cmd): Support viewing running build console text (#53)
    The console command `jen console` (without arguments) behavior has changed.
    * When a job has no running builds -> Print the last build console
    * When a job has only one running build -> Print the running build console
    * When a job has multiple running builds -> Ask user to pick a build
    
    Closes: #51

--- src/commands/__tests__/console.spec.js
+++ src/commands/__tests__/console.spec.js
@@ -1,11 +1,15 @@
-const { Readable, PassThrough } = require('stream');
+const EventEmitter = require('events');
+
+const { gray, red, yellow } = require('kleur');
 
 const { getCurrentBranchName } = require('../../lib/git-cmd');
-const { getConsoleText } = require('../../lib/jenkins');
-const { streamToString } = require('../../test-helper');
+const { createProgressiveTextStream, getRunningBuilds } = require('../../lib/jenkins');
+const { askToSelectARunningBuild } = require('../../lib/prompt');
 
 jest.mock('../../lib/jenkins');
 jest.mock('../../lib/git-cmd');
+jest.mock('../../lib/prompt');
+jest.mock('../../lib/log');
 
 const spinner = {
   start: jest.fn(),
@@ -17,65 +21,148 @@ jest.doMock('ora', () => {
   return jest.fn().mockImplementation(() => spinner);
 });
 
-const buildConsole = require('../console');
+const printConsole = require('../console');
 
 describe('console', () => {
   const branchName = 'foo';
+  let spiedStdWrite;
   beforeAll(() => {
     getCurrentBranchName.mockImplementation(() => branchName);
+    getRunningBuilds.mockImplementation(() => []);
+    spiedStdWrite = jest.spyOn(process.stdout, 'write').mockImplementation();
+  });
+
+  afterAll(() => {
+    spiedStdWrite.mockRestore();
   });
 
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it('should handle the exception when given build id does not exist', async () => {
-    const errorMessage = 'build does not exist';
-    getConsoleText.mockImplementation(() => Promise.reject(new Error(errorMessage)));
+  describe('build id not defined', () => {
+    const now = new Date().getTime();
+    const oneMinuteAgo = now - 1000 * 60;
+    const fiveMinuteAgo = now - 1000 * 60 * 5;
+    const mockedRunningBuilds = [
+      { id: '10', name: '#10', startTimeMillis: now },
+      { id: '11', name: '#11', startTimeMillis: oneMinuteAgo },
+      { id: '12', name: '#12', startTimeMillis: fiveMinuteAgo },
+    ];
 
-    const consoleOptions = { build: 404 };
-    await buildConsole(consoleOptions);
+    it('should select the latest build-id when there is no running builds', async () => {
+      const options = {};
+      await printConsole(options);
 
-    expect(spinner.start).toHaveBeenCalled();
-    expect(getConsoleText).toHaveBeenCalledWith(branchName, consoleOptions.build);
-    expect(spinner.fail).toHaveBeenCalledWith(errorMessage);
+      expect(createProgressiveTextStream).toHaveBeenCalledWith(branchName, 'lastBuild');
+    });
+
+    it('should pick the running build-id when there is only one running build', async () => {
+      getRunningBuilds.mockImplementation(() => [{ id: '100' }]);
+
+      const options = {};
+      await printConsole(options);
+
+      expect(createProgressiveTextStream).toHaveBeenCalledWith(branchName, '100');
+    });
+
+    it('should ask the user to select a build when there are multiple running builds', async () => {
+      const selectedBuildId = '10';
+      askToSelectARunningBuild.mockImplementation(() =>
+        Promise.resolve({ selectedBuildId })
+      );
+
+      getRunningBuilds.mockImplementation(() => mockedRunningBuilds);
+
+      const options = {};
+      await printConsole(options);
+
+      expect(askToSelectARunningBuild).toHaveBeenCalledWith([
+        { value: '10', title: `#10${gray(' (Started just now)')}` },
+        { value: '11', title: `#11${gray(' (Started a minute ago)')}` },
+        { value: '12', title: `#12${gray(' (Started 5 minutes ago)')}` },
+      ]);
+      expect(createProgressiveTextStream).toHaveBeenCalledWith(
+        branchName,
+        selectedBuildId
+      );
+    });
+
+    it('should cancel the command if the user canceled the prompt', async () => {
+      getRunningBuilds.mockImplementation(() => mockedRunningBuilds);
+      jest.spyOn(process, 'exit').mockImplementation();
+
+      askToSelectARunningBuild.mockImplementation(() =>
+        Promise.resolve({ selectedBuildId: undefined })
+      );
+
+      const options = {};
+      await printConsole(options);
+
+      expect(spiedStdWrite).toHaveBeenCalledWith(red('Aborted\n'));
+      expect(process.exit).toHaveBeenCalledTimes(1);
+    });
+
+    it('should handle exceptions when fetching running builds', async () => {
+      jest.spyOn(process, 'exit').mockImplementation();
+
+      const error = new Error('boom');
+      getRunningBuilds.mockImplementation(() => Promise.reject(error));
+
+      const options = {};
+      await printConsole(options);
+
+      expect(spinner.fail).toHaveBeenCalledWith('boom');
+      expect(process.exit).toHaveBeenCalledTimes(1);
+    });
   });
 
-  it('should stream content to writable stream', async () => {
-    const rs = new Readable();
-    rs.push('hello ');
-    rs.push('my name ');
-    rs.push('is foo');
-    rs.push(null);
-    getConsoleText.mockImplementation(() => Promise.resolve(rs));
+  it('should retrieve the build console', async done => {
+    const emitter = new EventEmitter();
 
-    const st = PassThrough();
-    const consoleOptions = { writeTo: st };
-    await buildConsole(consoleOptions);
+    let counter = 0;
+    const intervalId = setInterval(() => {
+      counter++;
+      emitter.emit('data', `${counter}: hello`);
 
-    expect(spinner.start).toHaveBeenCalled();
-    expect(spinner.stop).toHaveBeenCalled();
-    expect(await streamToString(st)).toBe('hello my name is foo');
-  });
+      if (counter === 3) {
+        emitter.emit('end');
+        clearInterval(intervalId);
+      }
+    }, 100);
 
-  it('should handle errors while streaming build console', async done => {
-    const spiedConsole = jest.spyOn(console, 'log').mockImplementationOnce(() => {});
+    createProgressiveTextStream.mockImplementation(() => emitter);
 
-    const streamingError = new Error('streaming failed');
-    const rs = new Readable();
-    rs._read = () => {
-      rs.destroy(streamingError);
-    };
-    getConsoleText.mockImplementation(() => Promise.resolve(rs));
-
-    const st = PassThrough();
-    const consoleOptions = { writeTo: st };
-    await buildConsole(consoleOptions);
+    const options = { build: '10' };
+    await printConsole(options);
 
     setTimeout(() => {
-      expect(spiedConsole).toHaveBeenCalledWith(streamingError);
-      expect(spinner.fail).not.toHaveBeenCalled();
+      expect(spiedStdWrite.mock.calls[0][0]).toBe(yellow('Build (#10) console\n\n'));
+      expect(spiedStdWrite.mock.calls[1][0]).toBe('1: hello');
+      expect(spiedStdWrite.mock.calls[2][0]).toBe('2: hello');
+      expect(spiedStdWrite.mock.calls[3][0]).toBe('3: hello');
       done();
-    });
+    }, 500);
+  });
+
+  it('should handle exceptions while retrieving the build console', async done => {
+    const emitter = new EventEmitter();
+
+    const intervalId = setInterval(() => {
+      emitter.emit('error', new Error('Boom'));
+      clearInterval(intervalId);
+    }, 100);
+
+    createProgressiveTextStream.mockImplementation(() => emitter);
+
+    const options = { build: '10' };
+    await printConsole(options);
+
+    setTimeout(() => {
+      expect(spinner.fail).toHaveBeenCalledWith(
+        'An error occurred while retrieving build console'
+      );
+      done();
+    }, 200);
   });
 });
--- src/commands/console.js
+++ src/commands/console.js
@@ -1,32 +1,104 @@
 const ora = require('ora');
-
-// pump is only necessary for Node.js 8.x or earlier (ATM Jenni requires 8.12).
-// For Node.js 10.x or later version we can use Native `stream.pipeline` method
-const pump = require('pump');
+const { red, yellow, gray } = require('kleur');
+const ago = require('s-ago');
 
 const { getCurrentBranchName } = require('../lib/git-cmd');
-const { debug } = require('../lib/log');
-const { getConsoleText } = require('../lib/jenkins');
+const { debug, logNetworkErrors } = require('../lib/log');
+const { getRunningBuilds, createProgressiveTextStream } = require('../lib/jenkins');
+const { askToSelectARunningBuild } = require('../lib/prompt');
 
 const spinner = ora();
+const LAST_BUILD = 'lastBuild';
 
-module.exports = async function buildConsole(options = {}) {
+function mapRunningBuilds(runningBuilds) {
+  let title = '';
+
+  return runningBuilds.map(({ id, name, startTimeMillis }) => {
+    title = name;
+
+    if (startTimeMillis) {
+      title += gray(` (Started ${ago(new Date(startTimeMillis), 'hour')})`);
+    }
+
+    return {
+      value: id,
+      title,
+    };
+  });
+}
+
+async function selectBuildIdFromRunningBuilds(runningBuilds = []) {
+  // No running builds, so print the last build
+  if (runningBuilds.length === 0) return LAST_BUILD;
+
+  if (runningBuilds.length === 1) return runningBuilds[0].id;
+
+  // Job has multiple running builds, so ask the user to select a build.
+  const { selectedBuildId } = await askToSelectARunningBuild(
+    mapRunningBuilds(runningBuilds)
+  );
+
+  return selectedBuildId;
+}
+
+function printStatus(buildId) {
+  if (buildId === LAST_BUILD) {
+    process.stdout.write(yellow('Latest build console\n\n'));
+    return;
+  }
+
+  process.stdout.write(yellow(`Build (#${buildId}) console\n\n`));
+}
+
+module.exports = async function printConsole(options = {}) {
   const branchName = getCurrentBranchName();
-  const buildId = options.build;
-  const output = options.writeTo || process.stdout;
+  let buildId = options.build;
+  debug(`Branch name: ${branchName}, build id: ${buildId || 'not defined'}`);
 
-  debug(`Branch name: ${branchName}, build: ${buildId || 'not defined'}`);
+  if (!buildId) {
+    debug('Build id not defined, so attempting to select a build id from running builds');
+
+    try {
+      spinner.start();
+      const runningBuilds = await getRunningBuilds(branchName);
+      spinner.stop();
+
+      const selectedBuildId = await selectBuildIdFromRunningBuilds(runningBuilds);
+
+      // user canceled the prompt
+      if (!selectedBuildId) {
+        process.stdout.write(red('Aborted\n'));
+        process.exit();
+      }
+
+      buildId = selectedBuildId;
+      debug(`Selected build id: ${buildId}`);
+    } catch (error) {
+      spinner.fail(error.message);
+      logNetworkErrors(error);
+      process.exit();
+    }
+  }
 
   try {
+    printStatus(buildId);
     spinner.start();
-    const consoleStream = await getConsoleText(branchName, buildId);
+    const stream = createProgressiveTextStream(branchName, buildId);
 
-    spinner.stop();
-    pump(consoleStream, output, error => {
-      if (error) {
-        debug('An error occurred while streaming build console');
-        console.log(error);
-      }
+    stream.on('data', data => {
+      spinner.stop();
+      process.stdout.write(data);
+      spinner.start();
+    });
+
+    stream.on('end', () => {
+      spinner.stop();
+    });
+
+    stream.on('error', error => {
+      process.stdout.write('\n');
+      spinner.fail('An error occurred while retrieving build console');
+      debug(error);
     });
   } catch (error) {
     spinner.fail(error.message);
